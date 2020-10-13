178465a80914a74be46a29af81f4d21df2fd4d86
Jeremy Rand <jeremyrand@airmail.cc>
2020-10-13 05:33:18 +0000

    Sort cert list
    Makes it easier to see progress by eye.

--- ctlpop.ps1
+++ ctlpop.ps1
@@ -27,7 +27,7 @@ $initial_cert_count = (& certutil -store AuthRoot | Select-String -Pattern "=== 
 & certutil -v -syncWithWU -f -f "$sync_dir"
 
 # Get the list of new certs
-$cert_files = Get-ChildItem "$sync_dir" -Filter "*.crt"
+$cert_files = Get-ChildItem "$sync_dir" -Filter "*.crt" | Sort-Object
 
 # Measure count of new certs
 $downloaded_cert_count = ($cert_files | Measure-Object -Line).Lines
