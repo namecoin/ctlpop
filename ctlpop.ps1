b0e0d2a13128b405e49663455e2c7965043d6729
Jeremy Rand <jeremyrand@airmail.cc>
2020-10-16 02:16:52 +0000

    Try something really dumb...

--- ctlpop.ps1
+++ ctlpop.ps1
@@ -23,49 +23,7 @@ param (
 # Measure initial count of certs
 $initial_cert_count = (& certutil -store AuthRoot | Select-String -Pattern "=== Certificate \d+ ===" | Measure-Object -Line).Lines + (& certutil -store Root | Select-String -Pattern "=== Certificate \d+ ===" | Measure-Object -Line).Lines
 
-# Download the new certs
-& certutil -v -syncWithWU -f -f "$sync_dir"
-If (!$?) {
-  Write-Host "syncWithWU failed!  Is your network connection down?"
-  exit 1
-}
-
-# Get the list of new certs
-$cert_files = Get-ChildItem "$sync_dir" -Filter "*.crt" | Sort-Object
-
-# Measure count of new certs
-$downloaded_cert_count = ($cert_files | Measure-Object -Line).Lines
-If (0 -eq $downloaded_cert_count) {
-  Write-Host "No certs downloaded from WU!  Is your network connection down?"
-  exit 1
-}
-
-# Import the new certs to the store
-:CERT foreach ($single_cert in $cert_files) {
-  Write-Host $single_cert.Name
-  foreach ( $TryNum in 1..10 ) {
-    & certutil -verify $single_cert.FullName | Set-Variable -Name verify_output
-    If (!$?) {
-      Write-Host "$verify_output"
-      Write-Host "Failed to import cert!  Retrying..."
-      Start-Sleep -seconds 5
-      continue
-    }
-    $cert_key_authroot = "HKLM:\SOFTWARE\Microsoft\SystemCertificates\AuthRoot\Certificates\" + $single_cert.Name.split(".")[0]
-    $cert_key_root = "HKLM:\SOFTWARE\Microsoft\SystemCertificates\Root\Certificates\" + $single_cert.Name.split(".")[0]
-    If ( -not (Test-Path "$cert_key_authroot") -and -not (Test-Path "$cert_key_root") ) {
-      Write-Host "$verify_output"
-      Write-Host "Import had no effect!  Retrying..."
-      & certutil -store AuthRoot $single_cert.Name.split(".")[0]
-      Get-ChildItem -path "HKLM:\SOFTWARE\Microsoft\" -recurse -ErrorAction SilentlyContinue | Where-Object {$_.Name -like ("*" + $single_cert.Name)}
-      Start-Sleep -seconds 5
-      continue
-    }
-    # Success with this cert, move onto the next cert.
-    continue CERT
-  }
-  exit 1
-}
+& certutil -f -verifyCTL AuthRootWU
 
 # Measure final count of certs
 $final_cert_count = (& certutil -store AuthRoot | Select-String -Pattern "=== Certificate \d+ ===" | Measure-Object -Line).Lines + (& certutil -store Root | Select-String -Pattern "=== Certificate \d+ ===" | Measure-Object -Line).Lines
@@ -73,6 +31,5 @@ $diff_cert_count = $final_cert_count - $initial_cert_count
 
 Write-Host "----- Results -----"
 Write-Host "----- Initial certs: $initial_cert_count -----"
-Write-Host "----- Downloaded certs: $downloaded_cert_count -----"
 Write-Host "----- Final certs: $final_cert_count -----"
 Write-Host "----- Diff (Final-Initial): $diff_cert_count -----"
